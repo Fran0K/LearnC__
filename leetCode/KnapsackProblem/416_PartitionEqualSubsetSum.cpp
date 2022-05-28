07ca9f94fb0f3e69094ece00146060d53dbc0d1d
Frank Lam <lhc1256744295@hotmail.com>
2022-05-28 23:21:18 +0800

    update README.md

--- leetCode/KnapsackProblem/416_PartitionEqualSubsetSum.cpp
+++ leetCode/KnapsackProblem/416_PartitionEqualSubsetSum.cpp
@@ -2,41 +2,40 @@
 // Created by Frank Lin on 28/5/2022.
 //
 #include "vector"
-
+#include "numeric"
 using namespace std;
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
         int n = nums.size();
-        int sum =0;
+        int sum =accumulate(nums.begin(),nums.end(),0);
 
         //计算总数
-        for(auto p:nums){
-            sum+=p;
-        }
+
         //如果是奇数
+        int target = sum/2;
         if(sum%2==1){
             return false;
         }
 
-        vector<vector<bool>>dp(n,vector<bool>(sum+1));
+        vector<vector<bool>>dp(n+1,vector<bool>(target+1));
         //base case;
         for(int i =0;i<n;i++){
             dp[i][0]= true;
         }
 
-        for(int i =1;i<=sum;i++){
-            for(int j= 0; j<n;j++){
+        for(int i =1;i<=n;i++){
+            for(int j= 1; j<=target;j++){
                 //能装
-                if(i-nums[j]>=0){
-                    dp[i][j] = dp[i][j]||dp[i-nums[j]][j];
+                if(j-nums[i-1]>=0){
+                    dp[i][j] = dp[i-1][j]||dp[i-1][i-nums[j]];
                 }else{
                     //不能装
                     dp[i][j] = dp[i-1][j];
                 }
             }
         }
-        return dp[sum][n-1];
+        return dp[target][n-1];
 
 
     }
